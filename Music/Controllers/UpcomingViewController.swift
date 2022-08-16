85f3135af364e73d467dedbb8bd502ff97c2ba8d
ElsieMay <elsiemaylawrie1@gmail.com>
2022-08-16 23:05:16 +1000

    Added upcoming view controller

---
+++ Music/Controllers/UpcomingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UpcomingViewController.swift
+//  Music
+//
+//  Created by Elsie May Lawrie on 16/8/2022.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemIndigo
+    }
+
+}
