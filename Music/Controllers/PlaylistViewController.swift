a19b876cbcf9a8d1f7fcc644d2c83d9673c4a8db
ElsieMay <elsiemaylawrie1@gmail.com>
2022-08-16 23:08:46 +1000

    Added playlist view controller

---
+++ Music/Controllers/PlaylistViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistViewController.swift
+//  Music
+//
+//  Created by Elsie May Lawrie on 16/8/2022.
+//
+
+import UIKit
+
+class PlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemOrange
+    }
+
+}
