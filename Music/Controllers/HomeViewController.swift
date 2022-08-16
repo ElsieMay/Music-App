57cb8d7f6bbdd8cf6ce3e6604557756556fbea4d
ElsieMay <elsiemaylawrie1@gmail.com>
2022-08-16 22:59:57 +1000

    Added search view controller

---
+++ Music/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Music
+//
+//  Created by Elsie May Lawrie on 16/8/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
---
+++ Music/Controllers/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  Music
+//
+//  Created by Elsie May Lawrie on 16/8/2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
